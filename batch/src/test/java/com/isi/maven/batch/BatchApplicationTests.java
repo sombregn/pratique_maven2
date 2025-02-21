cef5047ecac324595813f009f819ff745829188b
devGN224sn <souleymane.bah@universcitiz.com>
2025-02-21 23:46:43 +0000

    add

---
+++ batch/src/test/java/com/isi/maven/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isi.maven.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ web/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World M2Gl!</h2>
+</body>
+</html>
